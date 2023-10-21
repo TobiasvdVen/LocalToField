538a937ad386148fa14db031f41677e1fee8e2c3
Tobias van de Ven <tobiasvdven@hotmail.com>
2023-10-21 14:46:37 +0200

    No var

--- LocalToField/LocalToFieldCodeRefactoringProvider.cs
+++ LocalToField/LocalToFieldCodeRefactoringProvider.cs
@@ -3,9 +3,8 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
-using System.Collections.Generic;
 using System.Composition;
 using System.Linq;
 using System.Threading;
@@ -20,20 +19,20 @@ namespace LocalToField
         {
             // TODO: Replace the following code with your own analysis, generating a CodeAction for each refactoring to offer
 
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            SyntaxNode root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
             // Find the node at the selection.
-            var node = root.FindNode(context.Span);
+            SyntaxNode node = root.FindNode(context.Span);
 
             // Only offer a refactoring if the selected node is a type declaration node.
-            var typeDecl = node as TypeDeclarationSyntax;
+            TypeDeclarationSyntax typeDecl = node as TypeDeclarationSyntax;
             if (typeDecl == null)
             {
                 return;
             }
 
             // For any type declaration node, create a code action to reverse the identifier text.
-            var action = CodeAction.Create("Reverse type name", c => ReverseTypeNameAsync(context.Document, typeDecl, c));
+            CodeAction action = CodeAction.Create("Reverse type name", c => ReverseTypeNameAsync(context.Document, typeDecl, c));
 
             // Register this code action.
             context.RegisterRefactoring(action);
@@ -42,17 +41,17 @@ namespace LocalToField
         private async Task<Solution> ReverseTypeNameAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
         {
             // Produce a reversed version of the type declaration's identifier token.
-            var identifierToken = typeDecl.Identifier;
-            var newName = new string(identifierToken.Text.ToCharArray().Reverse().ToArray());
+            SyntaxToken identifierToken = typeDecl.Identifier;
+            string newName = new string(identifierToken.Text.ToCharArray().Reverse().ToArray());
 
             // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+            SemanticModel semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            INamedTypeSymbol typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
 
             // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+            Solution originalSolution = document.Project.Solution;
+            OptionSet optionSet = originalSolution.Workspace.Options;
+            Solution newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
 
             // Return the new solution with the now-uppercase type name.
             return newSolution;
